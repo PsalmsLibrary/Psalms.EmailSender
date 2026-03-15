d4cc824c60bcae7c1ce2df6242505cefabd86857
Gustavo Soares <sirgustavo4@gmail.com>
2026-03-15 19:34:28 -0300

    feat: Creating IoC

---
+++ Psalms.EmailSender/IoC.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Psalms.EmailSender.Models;
+using Psalms.EmailSender.Models.Interfaces;
+using Psalms.EmailSender.Services;
+using Psalms.EmailSender.Services.Interfaces;
+
+namespace Psalms.EmailSender;
+
+public static class IoC
+{
+    public static void AddPsalmsEmailSender(this IServiceCollection services)
+    {
+        services.AddScoped<IEmailService, EmailService>();
+        services.AddScoped<IEmailHtmlGenerator, EmailHtmlGenerator>();
+        services.AddScoped<IEmailTemplateResolver, EmailTemplateResolver>();
+
+        services.AddMediatR(
+            cfg => {
+                cfg.RegisterServicesFromAssembly(typeof(IEmailService).Assembly);
+            });
+    }
+}
